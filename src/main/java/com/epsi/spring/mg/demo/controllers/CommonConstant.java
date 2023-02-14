0cd21d125da4a524b32a311139501cd0e990d8e5
Mickael Gaillard <mickael.gaillard@gmail.com>
2023-02-14 10:48:28 +0100

    Add edit user
    Signed-off-by: Mickael Gaillard <mickael.gaillard@gmail.com>

---
+++ src/main/java/com/epsi/spring/mg/demo/controllers/CommonConstant.java
@@ -0,0 +1,10 @@
+package com.epsi.spring.mg.demo.controllers;
+
+public final class CommonConstant {
+
+    public static final String ROUTE_SHOW = "/show";
+    public static final String ROUTE_EDIT = "/edit";
+    public static final String ROUTE_SAVE = "/save";
+
+    private CommonConstant() { }
+}
--- src/main/java/com/epsi/spring/mg/demo/controllers/UserController.java
+++ src/main/java/com/epsi/spring/mg/demo/controllers/UserController.java
@@ -5,25 +5,46 @@ import java.time.LocalDateTime;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.epsi.spring.mg.demo.models.User;
 
 @Controller
-@RequestMapping("/users")
+@RequestMapping("/users")		// Prefix Path by /users/....
 public class UserController {
 
     public static final String USER_MODEL = "current_user";
 
-    @GetMapping("/mick")
+    private User currentUser;
+
+    public UserController() {
+        this.currentUser = new User()
+                .setFirstname("Mickael")
+                .setLastname("Gaillard")
+                .setDob(LocalDateTime.parse("2000-08-08T00:00:00"));
+    }
+
+    @GetMapping(CommonConstant.ROUTE_SHOW)   // => /users/show
+    //@RequestMapping(method = RequestMethod.GET, path = CommonConstant.ROUTE_SHOW)	// Base of @GetMapping(...)
     public String viewProfil(Model model) {
-        User user = new User()
-            .setFirstname("Mickael")
-            .setLastname("Gaillard")
-            .setDob(LocalDateTime.parse("2000-08-08T00:00:00"));
-
-
-        model.addAttribute(USER_MODEL, user);
+        model.addAttribute(USER_MODEL, this.currentUser);
         return "users/profil";
     }
+
+    @GetMapping(CommonConstant.ROUTE_EDIT)
+    public String editUser(Model model) {
+        model.addAttribute(USER_MODEL, this.currentUser);
+        return "users/form";
+    }
+
+    @PostMapping(CommonConstant.ROUTE_SAVE)
+    public String saveUser(Model model, @ModelAttribute User userSubmit) {
+        this.currentUser.setFirstname(userSubmit.getFirstname());
+        this.currentUser.setLastname(userSubmit.getLastname());
+
+        return "redirect:/users/show";
+    }
+
 }
