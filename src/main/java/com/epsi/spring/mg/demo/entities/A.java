6c38522c5d3aa04b1a8650cdbfc7cbcbdbb8caa7
Mickael Gaillard <mickael.gaillard@gmail.com>
2023-02-16 14:05:03 +0100

    Mapping Relational link
    Signed-off-by: Mickael Gaillard <mickael.gaillard@gmail.com>

---
+++ src/main/java/com/epsi/spring/mg/demo/entities/A.java
@@ -0,0 +1,31 @@
+package com.epsi.spring.mg.demo.entities;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tmp_a")
+public class A {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @ManyToMany(mappedBy = "relA")
+    private List<B> relB;
+
+    @ManyToOne
+    @JoinColumn(name = "relB1_ID", referencedColumnName = "id")
+    private B relB1;
+}
---
+++ src/main/java/com/epsi/spring/mg/demo/entities/B.java
@@ -0,0 +1,37 @@
+package com.epsi.spring.mg.demo.entities;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "tmp_b")
+public class B {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @ManyToMany
+    @JoinTable(
+            name="ab",
+            joinColumns = { @JoinColumn(name = "b_id") },
+            inverseJoinColumns = { @JoinColumn(name = "a_id")
+            }
+    )
+    private List<A> relA;
+
+    @OneToMany(mappedBy = "relB1")
+    private List<A> relA1;
+}
